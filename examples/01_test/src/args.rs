4658e55459b45eeb23c67a617d9773aa742ee308
Patrick Auernig <patrick.auernig@mykolab.com>
2018-02-09 18:11:37 +0100

    Simplify example

--- examples/01_test/src/args.rs
+++ examples/01_test/src/args.rs
@@ -4,55 +4,16 @@ pub const USAGE: &'static str = "
 WaniKani API test client.
 
 Usage:
-  01_test --key=<key> <command> [<id>]
+  01_test --key=<key>
   01_test [options]
 
 Options:
   -h, --help       Show this message
   -k, --key=<key>  The WaniKani API key
-
-Commands:
-  user
-  assignment <assignment-id>
-  assignments
-  subject <subject-id>
-  subjects
-  review-statistic <review-statistic-id>
-  review-statistics
-  study-material <study-material-id>
-  study-materials
-  summary
-  review <review-id>
-  reviews
-  level-progression <level-progression-id>
-  level-progressions
-  reset <reset-id>
-  resets
 ";
-
-macro_rules! match_and_print {
-    [ @matching ($args:ident: $cmd:ident => $call:expr, $($rest:tt)+) -> ($($out:tt)*) ] => {
-        match_and_print!(@matching
-            ($args: $($rest)+) -> ($($out)* stringify!($cmd) => { println!("{:#?}", $call); },)
-        )
-    };
-    [ @matching ($args:ident: $cmd:ident => $call:expr,) -> ($($out:tt)*) ] => {
-        match $args.arg_command.as_str() {
-            $($out)*
-            stringify!($cmd) => { println!("{:#?}", $call); },
-            _ => { println!("Unknown command!"); },
-        }
-    };
-
-    [ $($body:tt)* ] => {
-        match_and_print![@matching ($($body)*) -> ()];
-    };
-}
 
 #[derive(Debug, Deserialize)]
 pub struct Args {
-    pub arg_command: String,
-    pub arg_id:      Option<u32>,
     pub flag_key:    String,
 }
 
--- examples/01_test/src/main.rs
+++ examples/01_test/src/main.rs
@@ -3,7 +3,6 @@ extern crate serde_derive;
 extern crate docopt;
 extern crate wanikani;
 
-#[macro_use]
 mod args;
 use args::with_args;
 
@@ -12,25 +11,19 @@ use wanikani::Client as WaniKani;
 fn main() {
     with_args(|args| {
         let wk = WaniKani::configure(args.flag_key.to_owned());
-        let id = args.arg_id.unwrap_or_default().to_owned();
 
-        match_and_print![args:
-            user               => wk.user(),
-            summary            => wk.summary(),
-            subject            => wk.subject(id),
-            subjects           => wk.subjects(),
-            assignment         => wk.assignment(id),
-            assignments        => wk.assignments(),
-            review_statistic   => wk.review_statistic(id),
-            review_statistics  => wk.review_statistics(),
-            study_material     => wk.study_material(id),
-            study_materials    => wk.study_materials(),
-            review             => wk.review(id),
-            reviews            => wk.reviews(),
-            level_progression  => wk.level_progression(id),
-            level_progressions => wk.level_progressions(),
-            reset              => wk.reset(id),
-            resets             => wk.resets(),
-        ];
+        let user = wk.user();
+
+        println!("{:#?}", user);
+
+        let subjects1 = wk.subjects(|f| f
+            .levels(&vec![1])
+            .types(&vec!["kanji"]));
+        
+        println!("{:#?}", subjects1);
+
+        let assignment = wk.assignment(88214909);
+
+        println!("{:#?}", assignment);
     });
 }
